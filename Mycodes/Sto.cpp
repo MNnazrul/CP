c9c5391c0d9feefed4e1042f9c45cb1d72ae08ed
Nazrul Islam <69568084+MNnazrul@users.noreply.github.com>
2024-10-02 18:41:53 +0600

    Update Sto.cpp
    random. added

--- Mycodes/Sto.cpp
+++ Mycodes/Sto.cpp
@@ -72,4 +72,6 @@ int inversemod(int n) {
 // template <typename T> using oset = 
 // tree<T, null_type, less<T>, 
 // rb_tree_tag, tree_order_statistics_node_update>;
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
 
